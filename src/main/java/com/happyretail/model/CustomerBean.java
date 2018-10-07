050174c03ac2246b425a14432c7a93dbc265b9ce
user <user@email.com>
2018-10-07 12:23:49 +0530

    first commit by hemanth, added beans and modified application.properties

---
+++ src/main/java/com/happyretail/model/CustomerBean.java
@@ -0,0 +1,84 @@
+package com.happyretail.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="customers")
+public class CustomerBean {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="custId")
+	private String custId;
+	
+	@NotNull(message="Field should not be empty")
+	@Size(min=2,message="Name should have at least two characters")
+	@Column(name="custName")
+	private String custName;
+	
+	@NotNull(message="Field should not be empty")
+	@Pattern(regexp="\\w+@\\w+\\.\\w+",message="Email is invalid")
+	@Column(name="custEmail")
+	private String custEmail;
+	
+	@NotNull(message="Field should not be empty")
+	@Size(min=6,message="Password should have at least six characters")
+	@Column(name="custPass")
+	private String custPass;
+	
+	@NotNull(message="Field should not be empty")
+	@Size(min=10,message="Phone number should have at least 10 digits")
+	@Column(name="custPhone")
+	private String custPhone;
+	
+	public CustomerBean()
+	{
+		
+	}
+	public CustomerBean(String custId, String custName, String custEmail, String custPass, String custPhone) {
+		super();
+		this.custId = custId;
+		this.custName = custName;
+		this.custEmail = custEmail;
+		this.custPass = custPass;
+		this.custPhone = custPhone;
+	}
+	public String getCustId() {
+		return custId;
+	}
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
+	public String getCustName() {
+		return custName;
+	}
+	public void setCustName(String custName) {
+		this.custName = custName;
+	}
+	public String getCustEmail() {
+		return custEmail;
+	}
+	public void setCustEmail(String custEmail) {
+		this.custEmail = custEmail;
+	}
+	public String getCustPass() {
+		return custPass;
+	}
+	public void setCustPass(String custPass) {
+		this.custPass = custPass;
+	}
+	public String getCustPhone() {
+		return custPhone;
+	}
+	public void setCustPhone(String custPhone) {
+		this.custPhone = custPhone;
+	}
+
+}
---
+++ src/main/java/com/happyretail/model/ProductBean.java
@@ -0,0 +1,77 @@
+package com.happyretail.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="products")
+public class ProductBean {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="prodId")
+	private String prodId;
+	
+	@NotNull(message="Field should not be empty")
+	@Column(name="prodCat")
+	private String prodCat;
+	
+	@NotNull(message="Field should not be empty")
+	@Column(name="prodBrand")
+	private String prodBrand;
+	
+	@NotNull(message="Field should not be empty")
+	@Column(name="prodName")
+	private String prodName;
+	
+	@NotNull(message="Field should not be empty")
+	@Column(name="prodPrice")
+	private double prodPrice;
+	
+	public ProductBean() {
+		
+	}
+	public ProductBean(String prodId, String prodCat, String prodBrand, String prodName, double prodPrice) {
+		super();
+		this.prodId = prodId;
+		this.prodCat = prodCat;
+		this.prodBrand = prodBrand;
+		this.prodName = prodName;
+		this.prodPrice = prodPrice;
+	}
+	public String getProdId() {
+		return prodId;
+	}
+	public void setProdId(String prodId) {
+		this.prodId = prodId;
+	}
+	public String getProdCat() {
+		return prodCat;
+	}
+	public void setProdCat(String prodCat) {
+		this.prodCat = prodCat;
+	}
+	public String getProdBrand() {
+		return prodBrand;
+	}
+	public void setProdBrand(String prodBrand) {
+		this.prodBrand = prodBrand;
+	}
+	public String getProdName() {
+		return prodName;
+	}
+	public void setProdName(String prodName) {
+		this.prodName = prodName;
+	}
+	public double getProdPrice() {
+		return prodPrice;
+	}
+	public void setProdPrice(double prodPrice) {
+		this.prodPrice = prodPrice;
+	}
+
+}
