829ebc98b34719b27cb34ff168082480652561ed
argha <argha@nandan.com>
2018-10-07 10:18:01 +0530

    First commit from master

---
+++ src/main/java/com/happyretail/HappyRetailTeam2Application.java
@@ -0,0 +1,12 @@
+package com.happyretail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyRetailTeam2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyRetailTeam2Application.class, args);
+	}
+}
